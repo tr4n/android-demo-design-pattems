5bdd3090b35f2a3dfb538919d0c06143f657239c
Quang Huy Tran <32348171+tr4n@users.noreply.github.com>
2019-03-27 09:11:24 +0700

    Update singleton.java

--- singleton.java
+++ singleton.java
@@ -74,3 +74,16 @@ public class BillPughSingleton {
         return SingletonHelper.INSTANCE;
     }
 }
+public class ThreadSafeSingleton {
+    private static ThreadSafeSingleton instance;
+    private ThreadSafeSingleton(){}
+
+    public static ThreadSafeSingleton getInstance(){
+        if(instance == null){
+            synchronized(ThreadSafeSingleton.class){
+               instance = new ThreadSafeSingleton();
+            }
+        }
+        return instance;
+     }
+}
